71f998a2c779c3bf0388fa7b384e58a05659f017
nzhuk3 <zhuk.nikita.ev@gmail.com>
2025-03-03 09:36:18 +0300

    test commit

--- src/lib/core.js
+++ src/lib/core.js
@@ -139,7 +139,12 @@ function getComplexPA(target, prop, start, end) {
         endNum = res[0];
         chunk = end.substring(index, res.index);
         console.log(chunk);
-        
+
+
+
+        // Что-то написал вот так по бырому для коммита
+
+
 
         // НАПИСАТЬ СВОЁ ТОЧНО ТАКОЕ ЖЕ
         // if (endNum !== startNums[matchIndex++]) {
